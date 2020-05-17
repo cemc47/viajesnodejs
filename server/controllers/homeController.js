ec92deba51490aea3c563a4e94922e077296de3f
cemc47 <cemc47@gmail.com>
2020-05-16 20:38:37 -0600

    orden en testimoniales de inicio

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -4,7 +4,7 @@ const Testimonial = require('../models/Testimoniales');
 exports.consultasHomePage = async (req, res) =>{
     // multiples consultas
     const viajes = await Viaje.findAll({ limit: 3 });
-    const testimoniales = await Testimonial.findAll({ limit: 3 });
+    const testimoniales = await Testimonial.findAll({ order: [ ['id', 'DESC'] ], limit: 3 });
 
     // pasar el promise y ejecutarlo
     res.render('index', {
